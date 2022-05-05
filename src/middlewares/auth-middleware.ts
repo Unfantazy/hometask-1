3e6602039964b0ada67e3da022a9c354dd68d810
Angelina <angelina.kuptcova@gmail.com>
2022-05-05 13:45:51 +0300

    auth

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,7 +4,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const login = req.query.login
     const password = req.query.password
 
-    if (login !== 'admin' || password !== 'qwerty') {
+    if (login !== 'admin' || password !== 'qwerty' || req.headers.authorization?.split(' ')[0] !== 'Base') {
         res.sendStatus(401)
         return
     }
