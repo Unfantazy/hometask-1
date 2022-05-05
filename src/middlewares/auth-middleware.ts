78c98edce4bf7f830256375e973ee28ef88cb5bb
Angelina <angelina.kuptcova@gmail.com>
2022-05-05 13:46:49 +0300

    auth

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Response, Request } from 'express'
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const login = req.query.login
-    const password = req.query.password
+    // const login = req.query.login
+    // const password = req.query.password
 
-    if (login !== 'admin' || password !== 'qwerty' || req.headers.authorization?.split(' ')[0] !== 'Base') {
+    if (req.headers.authorization?.split(' ')[0] !== 'Base') {
         res.sendStatus(401)
         return
     }
