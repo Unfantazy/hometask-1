e43bcdec15dcd00d12b2457d5b74b2cd7b4019ca
Angelina <angelina.kuptcova@gmail.com>
2022-05-05 14:07:24 +0300

    auth

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,10 +1,25 @@
 import { NextFunction, Response, Request } from 'express'
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    // const login = req.query.login
-    // const password = req.query.password
+    if (!req.headers.authorization) {
+        res.sendStatus(401)
+        return
+    }
+    
+    if (req.headers.authorization.split(' ')[0] === 'Bearer') {
+        res.send(401)
+        return
+    }
 
-    if (req.headers.authorization?.split(' ')[0] !== 'Base') {
+    const token = req.headers.authorization.split(' ')[1]
+
+    const buff = new Buffer(token, 'base64')
+    const decodedToken = buff.toString('ascii').split(':')
+
+    const login = decodedToken[0]
+    const password = decodedToken[1]
+
+    if (login !== 'admin' || password !== 'qwerty') {
         res.sendStatus(401)
         return
     }
